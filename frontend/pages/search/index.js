8be0641685f79bcec3ef5b0c98ff5ae19378c03d
forsaken228 <1321890234@qq.com>
2018-05-21 21:44:02 +0800

    搜索页功能和 发布功能

--- frontend/pages/search/index.js
+++ frontend/pages/search/index.js
@@ -1,14 +1,34 @@
 // pages/search/index.js
 const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-  
+    inputShowed: false,
+    inputVal: ""
   },
-
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
