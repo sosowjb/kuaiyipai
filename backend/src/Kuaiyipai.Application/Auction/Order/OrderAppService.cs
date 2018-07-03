3bdb338bd8a3e887623c39a970d70ee6bbef35f7
Wang Jiabin <sosowjb@hotmail.com>
2018-07-03 21:03:24 +0800

    新增订单详情接口
    新增订单详情接口

--- backend/src/Kuaiyipai.Application/Auction/Order/OrderAppService.cs
+++ backend/src/Kuaiyipai.Application/Auction/Order/OrderAppService.cs
@@ -23,8 +23,10 @@ namespace Kuaiyipai.Auction.Order
         private readonly IRepository<OrderCompleted, Guid> _completedRepository;
         private readonly IRepository<User, long> _userRepository;
         private readonly IRepository<Entities.Address, Guid> _addressRepository;
+        private readonly IRepository<ItemCompleted, Guid> _itemRepository;
+        private readonly IRepository<ItemPic, Guid> _itemPicRepository;
 
-        public OrderAppService(IRepository<OrderWaitingForPayment, Guid> paymentRepository, IRepository<OrderWaitingForSending, Guid> sendingRepository, IRepository<OrderWaitingForReceiving, Guid> receivingRepository, IRepository<OrderWaitingForEvaluating, Guid> evaluatingRepository, IRepository<OrderCompleted, Guid> completedRepository, IRepository<User, long> userRepository, IRepository<Entities.Address, Guid> addressRepository)
+        public OrderAppService(IRepository<OrderWaitingForPayment, Guid> paymentRepository, IRepository<OrderWaitingForSending, Guid> sendingRepository, IRepository<OrderWaitingForReceiving, Guid> receivingRepository, IRepository<OrderWaitingForEvaluating, Guid> evaluatingRepository, IRepository<OrderCompleted, Guid> completedRepository, IRepository<User, long> userRepository, IRepository<Entities.Address, Guid> addressRepository, IRepository<ItemCompleted, Guid> itemRepository, IRepository<ItemPic, Guid> itemPicRepository)
         {
             _paymentRepository = paymentRepository;
             _sendingRepository = sendingRepository;
@@ -33,11 +35,13 @@ namespace Kuaiyipai.Auction.Order
             _completedRepository = completedRepository;
             _userRepository = userRepository;
             _addressRepository = addressRepository;
+            _itemRepository = itemRepository;
+            _itemPicRepository = itemPicRepository;
         }
 
         public async Task<PagedResultDto<GetWaitingForPaymentOrdersOutputDto>> GetWaitingForPaymentOrdersAsBuyer(GetWaitingForPaymentOrdersInputDto input)
         {
-            var query = _paymentRepository.GetAll().Where(o => o.BuyerId == AbpSession.UserId.Value && o.PaidTime==null);
+            var query = _paymentRepository.GetAll().Where(o => o.BuyerId == AbpSession.UserId.Value && o.PaidTime == null);
             if (!input.Sorting.IsNullOrEmpty())
             {
                 query = query.OrderBy(input.Sorting);
@@ -46,7 +50,7 @@ namespace Kuaiyipai.Auction.Order
             var count = await query.CountAsync();
             var list = await query.PageBy(input).Select(o => new GetWaitingForPaymentOrdersOutputDto
             {
-                Id=o.Id,
+                Id = o.Id,
                 Code = o.Code,
                 BuyerId = o.BuyerId,
                 SellerId = o.SellerId,
@@ -306,7 +310,7 @@ namespace Kuaiyipai.Auction.Order
 
                 var output = new GetEachTypeOrderCountOutputDto();
 
-                output.WaitPay = await _paymentRepository.CountAsync(t => t.SellerId == AbpSession.UserId.Value && t.PaidTime==null);
+                output.WaitPay = await _paymentRepository.CountAsync(t => t.SellerId == AbpSession.UserId.Value && t.PaidTime == null);
                 output.WaitReceive = await _receivingRepository.CountAsync(t => t.SellerId == AbpSession.UserId.Value);
                 output.WaitSend = await _sendingRepository.CountAsync(t => t.SellerId == AbpSession.UserId.Value);
 
@@ -316,6 +320,132 @@ namespace Kuaiyipai.Auction.Order
             {
                 throw e;
             }
+        }
+
+        public async Task<OrderDetailDto> GetOrder(Guid orderId)
+        {
+            if (!AbpSession.UserId.HasValue)
+            {
+                throw new UserFriendlyException("用户未登录");
+            }
+
+            OrderDetailDto order = new OrderDetailDto();
+            long sellerId, buyerId;
+            Guid goodsId;
+
+            var order5 = await _completedRepository.FirstOrDefaultAsync(orderId);
+            if (order5 == null)
+            {
+                var order4 = await _evaluatingRepository.FirstOrDefaultAsync(orderId);
+                if (order4 == null)
+                {
+                    var order3 = await _receivingRepository.FirstOrDefaultAsync(orderId);
+                    if (order3 == null)
+                    {
+                        var order2 = await _sendingRepository.FirstOrDefaultAsync(orderId);
+                        if (order2 == null)
+                        {
+                            var order1 = await _paymentRepository.FirstOrDefaultAsync(orderId);
+                            if (order1 == null)
+                            {
+                                throw new UserFriendlyException("找不到订单");
+                            }
+
+                            order.Id = order1.Id;
+                            order.OrderStatus = 1;
+                            order.OrderTime = order1.OrderTime;
+                            order.Price = order1.Amount;
+                            order.PaidTime = order1.PaidTime;
+                            order.AuctionNum = order1.Code;
+                            order.DeliveryType = "快递";
+                            order.DeliveryId = order1.DeliveryId;
+                            sellerId = order1.SellerId;
+                            buyerId = order1.BuyerId;
+                            goodsId = order1.ItemId;
+                        }
+                        else
+                        {
+                            order.Id = order2.Id;
+                            order.OrderStatus = 2;
+                            order.OrderTime = order2.OrderTime;
+                            order.Price = order2.Amount;
+                            order.AuctionNum = order2.Code;
+                            order.DeliveryType = "快递";
+                            order.DeliveryId = order2.DeliveryId;
+                            sellerId = order2.SellerId;
+                            buyerId = order2.BuyerId;
+                            goodsId = order2.ItemId;
+                        }
+                    }
+                    else
+                    {
+                        order.Id = order3.Id;
+                        order.OrderStatus = 3;
+                        order.OrderTime = order3.OrderTime;
+                        order.Price = order3.Amount;
+                        order.AuctionNum = order3.Code;
+                        order.DeliveryType = "快递";
+                        order.DeliveryId = order3.DeliveryId;
+                        sellerId = order3.SellerId;
+                        buyerId = order3.BuyerId;
+                        goodsId = order3.ItemId;
+                    }
+                }
+                else
+                {
+                    order.Id = order4.Id;
+                    order.OrderStatus = 4;
+                    order.OrderTime = order4.OrderTime;
+                    order.Price = order4.Amount;
+                    order.AuctionNum = order4.Code;
+                    order.DeliveryType = "快递";
+                    order.DeliveryId = order4.DeliveryId;
+                    sellerId = order4.SellerId;
+                    buyerId = order4.BuyerId;
+                    goodsId = order4.ItemId;
+                }
+            }
+            else
+            {
+                order.Id = order5.Id;
+                order.OrderStatus = 5;
+                order.OrderTime = order5.OrderTime;
+                order.Price = order5.Amount;
+                order.AuctionNum = order5.Code;
+                order.DeliveryType = "快递";
+                order.DeliveryId = order5.DeliveryId;
+                sellerId = order5.SellerId;
+                buyerId = order5.BuyerId;
+                goodsId = order5.ItemId;
+            }
+
+            var seller = await _userRepository.FirstOrDefaultAsync(sellerId);
+            order.SellerTel = seller.PhoneNumber;
+
+            var address = await _addressRepository.FirstOrDefaultAsync(a => a.CreatorUserId == buyerId && a.IsDefault);
+            order.BuyerName = address.Receiver;
+            order.BuyerTel = address.ContactPhoneNumber;
+            order.Address = address.Street;
+
+            var item = await _itemRepository.FirstOrDefaultAsync(goodsId);
+            if (item == null)
+            {
+                throw new UserFriendlyException("商品未找到");
+            }
+
+            order.GoodsName = item.Title;
+
+            var itemPic = await _itemPicRepository.FirstOrDefaultAsync(i => i.ItemId == goodsId && i.IsCover);
+            if (itemPic != null)
+            {
+                order.GoodsPicture = itemPic.Path;
+            }
+            else
+            {
+                order.GoodsPicture = "";
+            }
+
+            return order;
         }
 
         public async Task<GetEachTypeOrderCountOutputDto> GetEachTypeOrderCountAsBuyer()
@@ -331,7 +461,7 @@ namespace Kuaiyipai.Auction.Order
 
                 output.WaitPay = await _paymentRepository.CountAsync(t => t.BuyerId == AbpSession.UserId.Value && t.PaidTime == null);
                 output.WaitReceive = await _receivingRepository.CountAsync(t => t.BuyerId == AbpSession.UserId.Value);
-                output.WaitSend = await _sendingRepository.CountAsync(t =>t.BuyerId == AbpSession.UserId.Value);
+                output.WaitSend = await _sendingRepository.CountAsync(t => t.BuyerId == AbpSession.UserId.Value);
 
                 return output;
             }
