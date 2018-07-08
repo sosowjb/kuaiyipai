2017e81ee9dfd83783bc7996908ed0b81b6bc9ba
forsaken228 <1321890234@qq.com>
2018-07-09 00:06:18 +0800

    详情页，wy的货柜管理

--- frontend/app.js
+++ frontend/app.js
@@ -1,31 +1,6 @@
 //app.js
 App({
   onLaunch: function () {
-    // 登录
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-     // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-       /*  wx.getUserInfo({
-            withCredentials:true,
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              if (!wx.getStorageSync("accessToken"))
-              {
-                wx.login({
-                  success: res => {
-                  this.Logins(res.code,null)
-                  }
-                })
-              }
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })*/
-      }
-    })
   },
   Logins: function (nickname, avatarlink,code,callback){
     var that=this;
@@ -49,7 +24,11 @@ App({
               mask: true,
               duration: 500
             })
-        that.globalData.userInfo = { "avatar": avatarlink, "nickname": nickname};
+        that.globalData.userInfo = 
+        { 
+         "avatar": avatarlink,
+         "nickname": nickname
+        };
         wx.setStorage({
           key: "accessToken",
           data: res.data.result.accessToken
