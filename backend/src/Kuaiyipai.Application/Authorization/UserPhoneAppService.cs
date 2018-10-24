c6a6596591f54ff9bd1e6fbe5a97ab0312b9fad6
Wang Jiabin <sosowjb@hotmail.com>
2018-10-24 21:47:44 +0800

    新增短信服务
    新增短信服务

--- backend/src/Kuaiyipai.Application/Authorization/UserPhoneAppService.cs
+++ backend/src/Kuaiyipai.Application/Authorization/UserPhoneAppService.cs
@@ -79,8 +79,8 @@ namespace Kuaiyipai.Authorization
         const String domain = "dysmsapi.aliyuncs.com";
 
         // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-        const String accessKeyId = "yourAccessKeyId";
-        const String accessKeySecret = "yourAccessKeySecret";
+        const String accessKeyId = "LTAIaEP1GbiMUXa5";
+        const String accessKeySecret = "KluRss5a0t3EkW7bdXENw8tbPgEEMg";
         public SendSmsResponse SendSms(string phone, string captcha)
         {
             IClientProfile profile = DefaultProfile.GetProfile("cn-hangzhou", accessKeyId, accessKeySecret);
@@ -94,13 +94,13 @@ namespace Kuaiyipai.Authorization
                 //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
                 request.PhoneNumbers = phone;
                 //必填:短信签名-可在短信控制台中找到
-                request.SignName = "快易拍";
+                request.SignName = "快微拍";
                 //必填:短信模板-可在短信控制台中找到
-                request.TemplateCode = "SMS_1000000";
+                request.TemplateCode = "SMS_148612333";
                 //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-                request.TemplateParam = "{\"captcha\":\"" + captcha + "\"}";
+                request.TemplateParam = "{\"code\":\"" + captcha + "\"}";
                 //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
-                request.OutId = "yourOutId";
+                request.OutId = "1";
                 //请求失败这里会抛ClientException异常
                 response = acsClient.GetAcsResponse(request);
 
